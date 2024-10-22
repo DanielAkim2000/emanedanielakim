8e059d46376bfb6b5c86daf2990f2a2aadedb1e2
DanielAkim2000 <emanedanielakim@gmail.com>
2024-10-22 14:29:09 +0200

    fix du loader plus de border et animation debut a -90deg et fin a 270

--- src/components/loader/LoaderApp.jsx
+++ src/components/loader/LoaderApp.jsx
@@ -5,17 +5,11 @@ const LoaderApp = () => {
     return (
         <div className="w-full min-h-[24rem] max-h-full max-sm:max-h-[12rem] bg-transparent">
             <div className="w-full h-screen flex items-center justify-center backdrop-blur-lg  fixed z-20">
-                <div
-                    className="w-[15em] min-h-[15em] justify-center  p-5 rounded-3xl flex flex-col items-center gap-5
-                            shadow-2xl border-3 border-yellow-500 
-                        "
-                >
-                    <img
-                        src={loadingimg}
-                        alt="loading"
-                        className="loading-spin max-w-[3em]"
-                    />
-                </div>
+                <img
+                    src={loadingimg}
+                    alt="loading"
+                    className="loading-spin max-w-[3em]"
+                />
             </div>
         </div>
     );

6f012a1b216f2fb9709b02ee0159eb0bd54f2706
DanielAkim2000 <emanedanielakim@gmail.com>
2024-10-22 20:34:59 +0200

    ajustement du suspense pour que le loader ne cache pas le header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "@/App.css";
 import Layout from "@/pages/layout/Layout";
 import "animate.css";
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
-import LoaderApp from "./components/loader/LoaderApp";
 const Home = lazy(() => import("@/pages/Home"));
 const Projets = lazy(() => import("@/pages/Projets"));
 const Contact = lazy(() => import("@/pages/Contact"));
@@ -12,19 +11,17 @@ const Projet = lazy(() => import("@/pages/Projet"));
 
 function App() {
     return (
-        <Suspense fallback={<LoaderApp />}>
-            <Router>
-                <ScrollToTop />
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="projets" element={<Projets />} />
-                        <Route path="contact" element={<Contact />} />
-                        <Route path="projets/:id" element={<Projet />} />
-                    </Route>
-                </Routes>
-            </Router>
-        </Suspense>
+        <Router>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="projets" element={<Projets />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="projets/:id" element={<Projet />} />
+                </Route>
+            </Routes>
+        </Router>
     );
 }
 
--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,16 +1,19 @@
+import LoaderApp from "@/components/loader/LoaderApp";
 import Footer from "@/pages/layout/Footer";
 import Header from "@/pages/layout/Header";
-import React from "react";
+import React, { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
         <div className="relative w-full flex flex-col items-center  m-auto">
             <Header />
-            <main className="w-full">
-                <Outlet />
-            </main>
-            <Footer />
+            <Suspense fallback={<LoaderApp />}>
+                <main className="w-full">
+                    <Outlet />
+                </main>
+                <Footer />
+            </Suspense>
         </div>
     );
 };
